c195956051fb57b63ede18d993cb8c0b9f21961c
myWsq <wsq961@outlook.com>
2021-10-25 21:07:24 +0800

    feat: add star badge

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,7 @@ const ButtonGroup: React.FunctionComponent<{ gistUrl: string }> = ({
   gistUrl,
 }) => {
   return (
-    <div className="flex flex-col md:flex-row gap-3">
+    <div className="flex flex-col md:flex-row space-y-2 md:space-x-2 md:space-y-0">
       <Button color="primary" onClick={() => window.print()}>
         Export
       </Button>
@@ -64,8 +64,17 @@ const Home: NextPage<HomePageProps> = ({ user, config, error }) => {
   return (
     <div className="max-w-full w-min mx-auto self-center px-3 print:px-0 pb-12 print:pb-0">
       <nav className="print:hidden flex justify-between py-5 px-1">
-        <div className="flex">
+        <div className="flex items-center">
           <h1 className="text-xl font-bold text-primary-500">Resume Online</h1>
+          <a
+            href="https://github.com/myWsq/resume-online/stargazers"
+            className="ml-3"
+          >
+            <img
+              alt="GitHub stars"
+              src="https://img.shields.io/github/stars/myWsq/resume-online?style=social"
+            />
+          </a>
         </div>
         <div className="flex">
           <div className="hidden md:block">
@@ -81,7 +90,7 @@ const Home: NextPage<HomePageProps> = ({ user, config, error }) => {
       <div className="md:hidden mb-3 print:hidden">
         <ButtonGroup gistUrl={gistUrl}></ButtonGroup>
       </div>
-      <div className="border-2 border-gray-400 border-dashed print:border-0">
+      <div className="border-2 border-gray-400 border-dashed print:border-0 rounded">
         <ResumeRenderer md={config.md}></ResumeRenderer>
       </div>
     </div>
